69ef52b6ab1b7b665bd5833d9833ee8a3728a77d
UrfaKebab2929 <66250626+UrfaKebab2929@users.noreply.github.com>
2021-01-19 11:44:15 +0300

    dgfdsh
    sdfdshfs

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();//youtube.com/NoblesYT
+const fs = require("fs");
+const { Collection, Client } = require("discord.js");
+
+const client = new Client();//youtube.com/NoblesYT
+client.commands = new Collection();//youtube.com/NoblesYT
+client.queue = new Map()
+
+client.config = {
+  prefix: process.env.PREFIX
+}
+
+//Loading Events
+fs.readdir(__dirname + "/events/", (err, files) => {//youtube.com/NoblesYT
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));//youtube.com/NoblesYT
+    console.log("Event Yükleniyor: "+eventName)
+  });
+});
+
+//Loading Commands
+fs.readdir("./commands/", (err, files) => {//youtube.com/NoblesYT
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);//youtube.com/NoblesYT
+    console.log("Komut Yükleniyor: "+commandName)
+  });
+});
+
+//youtube.com/NoblesYT
+client.login(process.env.TOKEN)
